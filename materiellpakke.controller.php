496adf1fd847bd0048859ff5d7f5ee762c330b7a
Marius Mandal <mariusmandal@gmail.com>
2013-10-01 09:40:13 +0200

    Use data

--- materiellpakke.controller.php
+++ materiellpakke.controller.php
@@ -1,5 +1,14 @@
 <?php
+
+// FYLKE-INFOS
+$sql = new SQL("SELECT * FROM wp_materiell_fylke WHERE wp_materiell_fylke.fylke_id = '".get_option('fylke')."'");
+$fylkeinfos = $sql->run('array');
+foreach($res as $key => $val) {
+	$newkey = str_replace('fylke_','', $key);
+	$fylkedata[$newkey] = utf8_encode($val);
+}
 $infos = array('site_type' => get_option('site_type'),
 		   'season' => get_option('season'),
 		   'deadline' => '21.10.2013'
+		   'fylke' => $fylkedata;
 		  );
