0ee76c3e44ca2925bae9b79209a2d425f0bcebe7
Marius Mandal <mariusmandal@gmail.com>
2013-10-17 09:58:30 +0200

    class materiell

--- ukmnorge.controller.php
+++ ukmnorge.controller.php
@@ -1,4 +1,6 @@
 <?php
+require_once('class.materiell.php');
+
 $fylker = new SQL("SELECT `id` FROM `smartukm_fylke`
 				   WHERE `id` < 21");
 $fylker = $fylker->run();
@@ -8,54 +10,3 @@ while( $r = mysql_fetch_assoc( $fylker ) ) {
 
 
 $INFOS = array('fylker' => $fylkemateriell);
-
-
-
-
-class materiell {
-	
-	private function _season() {
-		$this->season = get_option('season');
-	}
-
-	public function __construct($fylke_id) {
-		$this->_season();
-		$qry = new SQL("SELECT *
-						FROM `wp_materiell_fylke`
-						WHERE `fylke_id` = '#fylke_id'",
-						array('fylke_id' => $fylke_id));
-		$res = $qry->run('array');
-		foreach($res as $key => $val) {
-			$newkey = str_replace('fylke_','', $key);
-			if(is_string($val))
-				$this->$newkey = utf8_encode($val);
-			else
-				$this->$newkey = $val;
-		}
-		
-		if( strpos($this->tid, ($this->season-1).'-') !== 0) {
-			$this->oppdatert = false;
-		}
-	}
-	
-	
-	public function load_kommuner() {
-		$qry = new SQL("SELECT *
-						FROM `wp_materiell`
-						WHERE `fylke_id` = '#fylke_id'",
-						array('fylke_id' => $this->id));
-		$res = $qry->run();
-		
-		while($r = mysql_fetch_assoc( $res ) ) {
-			$kommune = array();
-			foreach($r as $key => $val) {
-				if(is_string($val))
-					$kommune[$key] = utf8_encode($val);
-				else
-					$kommune[$key] = $val;
-			}
-			
-			$this->kommuner[] = $kommune;
-		}
-	}
-}
