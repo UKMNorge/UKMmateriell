7ccd472fb6012a9e765aa88fa1d9ecb1d83c923f
Marius Mandal <mariusmandal@gmail.com>
2013-10-17 11:02:38 +0200

    antall pakker

--- pakkeinnhold.controller.php
+++ pakkeinnhold.controller.php
@@ -1,11 +1,23 @@
 <?php
 require_once('class.materiell.php');
 
+// BEREGN PAKKER 
 $pakke_mini = new materiellpakke('mini');
 $pakke_medium = new materiellpakke('medium');
 $pakke_stor = new materiellpakke('stor');
 $pakke_fylke = new materiellpakke('fylke');
 
+$pakker = array('mini' 	=> $pakke_mini,
+				'medium'=> $pakke_medium,
+				'stor'	=> $pakke_stor,
+				'fylke' => $pakke_fylke);
+// Beregn antall av hver pakke
+foreach($pakker as $index => $pakke)
+	$pakker[$index]->antall();
+
+
+
+// HENT ALLE PRODUKTER
 $sql = new SQL("SELECT *
 				FROM `wp_materiell_produkt`
 				ORDER BY `produkt_navn` ASC");
@@ -21,4 +33,4 @@ while( $r = mysql_fetch_assoc( $res ) ) {
 }
 
 
-$infos = array('produkter' => $produkter);
+$infos = array('produkter' => $produkter, 'pakker' => $pakker);
