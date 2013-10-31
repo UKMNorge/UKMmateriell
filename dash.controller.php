5649777c517f51245cafffb6e605aad03f1cc20c
Marius Mandal <mariusmandal@gmail.com>
2013-10-31 11:39:18 +0100

    syntax

--- dash.controller.php
+++ dash.controller.php
@@ -6,11 +6,11 @@ $cuid = $current_user->ID;
 $bruker = $wpdb->get_row("SELECT `b_id`,`lock_email`, `b_email` FROM `ukm_brukere`
 						  WHERE `wp_bid` = '".$cuid."'");	
 
-if($bruker['lock_email'] == 'true' && strpos($bruker['b_email'], '@urg.ukm.no') !== false) {
+if($bruker->lock_email == 'true' && strpos($bruker->b_email, '@urg.ukm.no') !== false) {
 	$bruker = $wpdb->get_row("SELECT `b_id` FROM `ukm_brukere`
 							  WHERE `b_email` = '%s'
 							  AND `lock_email` = 'true'",
-							  str_replace('@urg.ukm','@ukm', $bruker['b_email']));
+							  str_replace('@urg.ukm','@ukm', $bruker->b_email));
 }
 
 $infos = array('user_id' => $bruker->b_id,
