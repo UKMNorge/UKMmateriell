3458eb38df8954617675c1a9fcc92e4849b68b6b
Marius Mandal <mariusmandal@gmail.com>
2013-10-31 11:45:50 +0100

    debug

--- dash.controller.php
+++ dash.controller.php
@@ -7,10 +7,12 @@ $bruker = $wpdb->get_row("SELECT `b_id`,`lock_email`, `b_email` FROM `ukm_bruker
 						  WHERE `wp_bid` = '".$cuid."'");	
 
 if($bruker->lock_email == 'true' && strpos($bruker->b_email, '@urg.ukm.no') !== false) {
-	$bruker = $wpdb->get_row("SELECT `b_id` FROM `ukm_brukere`
-							  WHERE `b_email` = '". str_replace('@urg.ukm','@ukm', $bruker->b_email) ."'
-							  AND `lock_email` = 'true'"
-							 );
+	$sql = "SELECT `b_id` 
+			FROM `ukm_brukere`
+			WHERE `b_email` = '". str_replace('@urg.ukm','@ukm', $bruker->b_email) ."'
+			AND `lock_email` = 'true'";
+			echo $sql;
+	$bruker = $wpdb->get_row($sql);							 );
 }
 
 $infos = array('user_id' => $bruker->b_id,
