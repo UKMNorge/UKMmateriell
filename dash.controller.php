9028240d743e98c13fec205f745730e5987faade
Marius Mandal <mariusmandal@gmail.com>
2013-11-27 12:55:13 +0100

    instrato public

--- dash.controller.php
+++ dash.controller.php
@@ -6,6 +6,15 @@ $cuid = $current_user->ID;
 $bruker = $wpdb->get_row("SELECT `b_id`,`lock_email`, `b_email` FROM `ukm_brukere`
 						  WHERE `wp_bid` = '".$cuid."'");	
 
+
+$infos = array('user_id' => $bruker->b_id,
+		   'user_key'	=> md5($bruker->b_id . UKM_INSTRATO_PEPPER),
+		   'site_type' => get_option('site_type'),
+		   'season' => get_option('season'),
+		   'deadline' => '21.10.2013'
+		  );
+
+
 if($bruker->lock_email == 'true' && strpos($bruker->b_email, '@urg.ukm.no') !== false) {
 	$fylkenavn = str_replace('@urg.ukm','@ukm', $bruker->b_email);
 	$bruker = $wpdb->get_row("SELECT `b_id` 
@@ -33,12 +42,5 @@ if($bruker->lock_email == 'true' && strpos($bruker->b_email, '@urg.ukm.no') !== 
 							'oppland-ekstern' => 'tilfredsstille52',
 							'hedmark-ekstern' => 'aksjoner44' );
 	$infos['eksternbruker']['navn'] = $fylkenavn.'-ekstern';
-	$infos['eksternbruker']['pass'] = $eksternbrukere[$fylkenavn.'-ekstern'];
+	$infos['eksternbruker']['pass'] = $eksternbrukere[strtolower($fylkenavn).'-ekstern'];
 }
-
-$infos = array('user_id' => $bruker->b_id,
-		   'user_key'	=> md5($bruker->b_id . UKM_INSTRATO_PEPPER),
-		   'site_type' => get_option('site_type'),
-		   'season' => get_option('season'),
-		   'deadline' => '21.10.2013'
-		  );
